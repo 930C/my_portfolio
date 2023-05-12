d43a096776e02e4ec9168bddb0c23130115bb91d
930C <93C@pm.me>
2023-05-12 14:55:10 +0200

    finish first iteration

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ console.log("Test")
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM CONTENT LOADED")
 })
+
+function addToList() {
+    var inputValue = document.getElementById("myText").value;
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(inputValue));
+    document.getElementById("hobbies_list").appendChild(li);
+}
